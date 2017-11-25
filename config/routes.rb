60feb045be4e3f2289544376ccc67ee645559bfc
DuWayne Petty <x17106672@student.ncirl.ie>
2017-11-25 20:37:05 +0000

    User profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :profiles
   devise_for :users
   root :to =>'home#index'  
   get 'home/index'
   
-  
+  get '/signedinuserprofile' => 'profiles#signedinuserprofile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
