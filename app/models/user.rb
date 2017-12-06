53dd2d66ad30c7813673afd3f287c093fa66ddb8
DuWayne Petty <x17106672@student.ncirl.ie>
2017-12-06 08:54:16 +0000

    taking trackable out of App

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,12 +18,6 @@ class User < ApplicationRecord
     save!(:validate => false)
   end
   
-  private
   
-  def confirmation_token
-    if self.confirm_token.blank?
-      self.confirm_token = SecureRandom.urlsafe_base64.to_s
-    end
-  end
   
 end
